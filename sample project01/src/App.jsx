0b0104e41bbd3c525b2235cef0cfd97c31699e20
PiyumalNipuna60 <nipuna315np@gmail.com>
2024-01-27 19:36:14 +0530

    funtion cll and variable call implementation

--- sample project01/src/App.jsx
+++ sample project01/src/App.jsx
@@ -1,10 +1,20 @@
-
-
 function App() {
   
+  const a=10;
+  const fName = "piyumal";
+  const lName="nipuna";
+  const contry="sri lankan"
+  const getFullName=(f,l) =>{
+    return `${f} ${l}`;
+  }
+  
   return (
-    <div>
-      <h1>Main Component</h1>
+    <div className="heading cont">
+      <h1 >Main Component</h1>
+      <h1>age : {a}</h1>
+      <p>full name : {fName} {lName}</p>
+      <p>full name (function): {getFullName(fName,lName)}</p>
+      <p>Contry : {contry}</p>
     </div>
   );
 }
