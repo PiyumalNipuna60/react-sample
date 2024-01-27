2e9e2877fdeb244a36d3444a764bb31c48ed551b
PiyumalNipuna60 <nipuna315np@gmail.com>
2024-01-27 19:42:00 +0530

    array declre and use

--- sample project01/src/App.jsx
+++ sample project01/src/App.jsx
@@ -4,6 +4,9 @@ function App() {
   const fName = "piyumal";
   const lName="nipuna";
   const contry="sri lankan"
+
+const arr=["Apple","Mango","Orange'"]
+
   const getFullName=(f,l) =>{
     return `${f} ${l}`;
   }
@@ -15,6 +18,7 @@ function App() {
       <p>full name : {fName} {lName}</p>
       <p>full name (function): {getFullName(fName,lName)}</p>
       <p>Contry : {contry}</p>
+      <p>{fName} like to eat  {arr[0]}</p>
     </div>
   );
 }
